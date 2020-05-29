d142715d3e3b84213a06a0f362890c74d528ecf4
Fenraz <graarf@mail.ru>
2020-05-29 17:06:57 +0300

    Реструктуризация файлов
    - Класс Worker вынесен в отдельный файл

---
+++ Fenraz.ShiftPayment/Worker.cs
@@ -0,0 +1,42 @@
+﻿using System;
+
+
+namespace Fenraz.ShiftPayment
+{
+    class Worker
+    {
+        string Name { get; set; }
+        uint Shifts { get; set; } // Количество отработанных смен
+        double Payment { get; set; } // Выплата после разделения
+
+        public Worker(string n, uint s)
+        {
+            Name = n;
+            Shifts = s;
+        }
+
+        // Отобразить данные по сотруднику
+        public void ShowWorker(Worker w)
+        {
+            Console.WriteLine("Имя: {0}, отработал смен: {1}", w.Name, w.Shifts);
+        }
+
+        // Отобразить данные после расчетов
+        public void ShowPayments(Worker w)
+        {
+            Console.WriteLine("Сотрудник: {0}, выплата: {1:f3}", w.Name, w.Payment);
+        }
+
+        // Добавление суммы
+        public void AddPayment(double sum)
+        {
+            Payment += sum;
+        }
+
+        // Возвратить количество смен (для расчетов)
+        public uint ReturnShifts(Worker w)
+        {
+            return w.Shifts;
+        }
+    }
+}
