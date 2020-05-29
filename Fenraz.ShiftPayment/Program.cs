4a410169749a53d6dbf54040ccd570e04152532d
Fenraz <graarf@mail.ru>
2020-05-29 18:43:15 +0300

    Реорганизация программы
    - Весь алгоритм расчета разделен на методы и вынесен
    в отдельный класс Payment для последующей организации меню

--- Fenraz.ShiftPayment/Program.cs
+++ Fenraz.ShiftPayment/Program.cs
@@ -5,11 +5,134 @@ using System.Text;
 
 namespace Fenraz.ShiftPayment
 {
-
-    class Program
+    class Payment
     {
+        uint Workers{ get; set; }
+
+        uint Shifts{ get; set; }
+
+        double OverallSum { get; set; }
+
+        public void AddWrkrsAmount(Payment obj)
+        {
+            Console.WriteLine("Введите количество рабочих, " +
+                "для которых будет производиться расчет");
+            obj.Workers = uint.Parse(GetLineNoLetters());
+            Console.WriteLine("Вы ввели: " + obj.Workers + "\n");
+        }
+
+        public void AddShiftsAmount(Payment obj)
+        {
+            Console.WriteLine("Введите общее количество смен в месяце.");
+
+            obj.Shifts = UInt32.Parse(GetLineNoLetters());
+
+            Console.WriteLine("Вы ввели: " + obj.Shifts + "\n");
+        }
+
+        public void AddOverallSum(Payment obj)
+        {
+            Console.WriteLine("Введите общую сумму выплат, полученных " +
+                "за совмещение профессий.");
+            obj.OverallSum = GetLineDouble();
+
+            Console.WriteLine("Вы ввели: " + obj.OverallSum + "\n");
+        }
+
+        public List<Worker> CreateWrkrsList()
+        {
+            // Добавление сотрудников
+            List<Worker> workersList = new List<Worker>();
+
+            for (int i = 1; i < Workers + 1; i++)
+            {
+                string n;
+                uint s = 0;
+                Console.WriteLine("Добавление сотрудника #" + i);
+
+                Console.WriteLine("Введите имя сотрудника:");
+                n = Console.ReadLine();
+
+                // Ввод количества смен, отработанных сотрудником
+                while (s == 0 || s > Shifts)
+                {
+                    try
+                    {
+                        Console.WriteLine("Введите количество смен, отработанных " +
+                            " сотрудником за месяц: ");
+                        s = UInt32.Parse(GetLineNoLetters());
+
+                        if (s > Shifts)
+                        {
+                            Console.WriteLine("Ошибка! Число смен, отработанных сотрудником" +
+                                " превышает общее количество смен.\n");
+                        }
+                        else if (s == 0)
+                        {
+                            Console.WriteLine("Ошибка! Число смен не может быть равно нулю.\n" +
+                                "Если сотрудник в этом месяце не работал, " +
+                                "Перезапустите программу и не добавляйте его в список.\n");
+                        }
+                        else
+                        {
+                            workersList.Add(new Worker(n, s));
+                            Console.WriteLine();
+                            break;
+                        }
+                    }
+                    catch (FormatException)
+                    {
+                        Console.WriteLine("Ошибка! Введенное значение не является числом.\n");
+                    }
+                }
+            }
+
+            return workersList;
+        }
+
+        public void ShowWrkrsList(List<Worker> list)
+        {
+            // Вывод списка сотрудников на экран
+            Console.WriteLine("Составлен следующий список сотрудников:");
+            foreach (Worker w in list)
+            {
+                w.ShowWorker(w);
+            }
+
+        }
+
+        public void CalculatePayment(List<Worker> list)
+        {
+            //Блок расчета. По Окончанию тестов вынести в отдельный метод
+            while (OverallSum > 1)
+            {
+                double payPerShift = (OverallSum / Workers) / Shifts;
+
+                foreach (Worker w in list)
+                {
+                    double sum = payPerShift * w.ReturnShifts(w);
+                    w.AddPayment(sum);
+                    OverallSum -= sum;
+                }
+
+            }
+
+            double finalPayment = OverallSum / Workers;
+            foreach (Worker w in list)
+            {
+                w.AddPayment(finalPayment);
+            }
+
+            // Список выплат
+            Console.WriteLine("Список выплат сотрудникам:");
+            foreach (Worker w in list)
+            {
+                w.ShowPayments(w);
+            }
+        }
+
         // Метод, запрещающий ввод каких-либо символов кроме цифр
-        private static string GetLineNoLetters()
+        public static string GetLineNoLetters()
         {
             var sb = new StringBuilder();
             while (true)
@@ -62,119 +185,30 @@ namespace Fenraz.ShiftPayment
                 }
             }
             return result;
-            
 
-            
+
+
         }
+    }
 
+    class Program
+    {              
         static void Main(string[] args)
         {
-            // Определение количества сотрудников
-            int workers;
+            Payment one = new Payment();
 
-            Console.WriteLine("Введите количество рабочих, " +
-                "для которых будет производиться расчет");
+            one.AddWrkrsAmount(one);
 
-            workers = Int32.Parse(GetLineNoLetters());
+            one.AddShiftsAmount(one);
 
-            Console.WriteLine("Вы ввели: " + workers + "\n");
+            List<Worker> workersList = one.CreateWrkrsList();
 
-            // Добавление общего числа смен в месяце
-            int shifts;
-            Console.WriteLine("Введите общее количество смен в месяце.");
+            one.ShowWrkrsList(workersList);
 
-            shifts = Int32.Parse(GetLineNoLetters());
+            one.AddOverallSum(one);
 
-            Console.WriteLine("Вы ввели: " + shifts + "\n");
-
-            // Добавление сотрудников
-            List<Worker> workersList = new List<Worker>();
-
-            for (int i = 1; i < workers+1; i++)
-            {
-                string n;
-                uint s = 0;
-                Console.WriteLine("Добавление сотрудника #" + i);
-
-                Console.WriteLine("Введите имя сотрудника:");
-                n = Console.ReadLine();
-
-                // Ввод количества смен, отработанных сотрудником
-                while (s == 0 || s > shifts)
-                {
-                    try
-                    {
-                        Console.WriteLine("Введите количество смен, отработанных " +
-                            " сотрудником за месяц: ");
-                        s = UInt32.Parse(GetLineNoLetters());
-
-                        if (s > shifts)
-                        {
-                            Console.WriteLine("Ошибка! Число смен, отработанных сотрудником" +
-                                " превышает общее количество смен.\n");
-                        }
-                        else if (s == 0)
-                        {
-                            Console.WriteLine("Ошибка! Число смен не может быть равно нулю.\n" +
-                                "Если сотрудник в этом месяце не работал, " +
-                                "Перезапустите программу и не добавляйте его в список.\n");
-                        }
-                        else
-                        {
-                            workersList.Add(new Worker(n, s));
-                            Console.WriteLine();
-                            break;
-                        }
-                    }
-                    catch (FormatException)
-                    {
-                        Console.WriteLine("Ошибка! Введенное значение не является числом.\n");
-                    }
-                }
-            }
-
-            // Вывод списка сотрудников на экран
-            Console.WriteLine("Составлен следующий список сотрудников:");
-            foreach (Worker w in workersList)
-            {
-                w.ShowWorker(w);
-            }
-
-            Console.WriteLine();
-
-            //Добавление суммы, полученной за совмещение
-            Console.WriteLine("Введите общую сумму выплат, полученных " +
-                "за совмещение профессий.");
-            double overallSum = GetLineDouble();
-
-            Console.WriteLine("Вы ввели: " + overallSum + "\n");
-
-            //Блок расчета. По Окончанию тестов вынести в отдельный метод
-            while(overallSum > 1)
-            {
-                double payPerShift = (overallSum / workers) / shifts;
-
-                foreach (Worker w in workersList)
-                {
-                    double sum = payPerShift * w.ReturnShifts(w);
-                    w.AddPayment(sum);
-                    overallSum -= sum;
-                }
-
-            }
-
-            double finalPayment = overallSum / workers;
-            foreach(Worker w in workersList)
-            {
-                w.AddPayment(finalPayment);
-            }
-
-            // Список выплат
-            Console.WriteLine("Список выплат сотрудникам:");
-            foreach(Worker w in workersList)
-            {
-                w.ShowPayments(w);
-            }
+            Console.WriteLine("Произвести расчет");
+            one.CalculatePayment(workersList);
         }
     }
 }
